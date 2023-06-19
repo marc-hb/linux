8d1b7d8f01ad36c64ce9ba9b965b8326f6b5ab82
Tao Su <tao1.su@linux.intel.com>
2023-06-19 16:53:14 +0800

    KVM: x86: Advertise AMX-FP8 CPUID to user space
    AMX-FP8 is a new set of instructions in the latest Intel platform
    Diamond Rapids, for supporting FP8 datatype for AI usage.
    
    The bit definition:
    CPUID.(EAX=7,ECX=1):ECX[bit 3]
    
    AMX-FP8 is on a new and sparse CPUID leaf and all bits on this leaf
    have no truly kernel use case for now. Given that and to save space
    for kernel feature bits, move this new leaf to KVM-only subleaf and
    plus an x86_FEATURE definition for AMX-FP8 to direct it to the KVM
    entry.
    
    Advertise AMX-FP8 to KVM userspace. This is safe because there are
    no new VMX controls or additional host enabling required for guests
    to use this feature.
    
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>

--- arch/x86/kvm/reverse_cpuid.h
+++ arch/x86/kvm/reverse_cpuid.h
@@ -25,6 +25,9 @@
 #define KVM_X86_FEATURE_SGX2		KVM_X86_FEATURE(CPUID_12_EAX, 1)
 #define KVM_X86_FEATURE_SGX_EDECCSSA	KVM_X86_FEATURE(CPUID_12_EAX, 11)
 
+/* Intel-defined sub-features, CPUID level 0x00000007:1 (ECX) */
+#define X86_FEATURE_AMX_FP8             KVM_X86_FEATURE(CPUID_7_1_ECX, 3)
+
 /* Intel-defined sub-features, CPUID level 0x00000007:1 (EDX) */
 #define X86_FEATURE_AVX_VNNI_INT8       KVM_X86_FEATURE(CPUID_7_1_EDX, 4)
 #define X86_FEATURE_AVX_NE_CONVERT      KVM_X86_FEATURE(CPUID_7_1_EDX, 5)
@@ -82,6 +85,7 @@ static const struct cpuid_reg reverse_cpuid[] = {
 	[CPUID_8000_0022_EAX] = {0x80000022, 0, CPUID_EAX},
 	[CPUID_7_2_EDX]       = {         7, 2, CPUID_EDX},
 	[CPUID_24_0_EBX]      = {      0x24, 0, CPUID_EBX},
+	[CPUID_7_1_ECX]       = {         7, 1, CPUID_ECX},
 };
 
 /*
