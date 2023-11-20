64ea776372c02f934280b90f1a7847e978359f96
Xin Li <xin3.li@intel.com>
2023-11-19 16:00:13 -0800

    KVM: VMX: Invoke vmx_set_cpu_caps() before nested setup
    Set VMX CPU capabilities before initializing nested instead of after.
    
    Signed-off-by: Xin Li <xin3.li@intel.com>
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>
    Tested-by: Shan Kang <shan.kang@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -8786,6 +8786,12 @@ __init int vmx_hardware_setup(void)
 
 	setup_default_sgx_lepubkeyhash();
 
+	/*
+	 * VMX CPU capabilities are required to setup the VMX basic MSR for
+	 * nested, so this must be done before nested_vmx_setup_ctls_msrs().
+	 */
+	vmx_set_cpu_caps();
+
 	if (nested) {
 		nested_vmx_setup_ctls_msrs(&vmcs_config, vmx_capability.ept);
 
@@ -8793,8 +8799,6 @@ __init int vmx_hardware_setup(void)
 		if (r)
 			return r;
 	}
-
-	vmx_set_cpu_caps();
 
 	r = alloc_kvm_area();
 	if (r && nested)
