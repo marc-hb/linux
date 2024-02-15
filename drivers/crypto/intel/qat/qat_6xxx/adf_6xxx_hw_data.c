9c8ea91d51d36bb57fb3fa15c2bb598bebf29425
George Abraham P <george.abraham.p@intel.com>
2024-02-15 21:02:57 +0530

    crypto: qat - add module parameter to enable fw heartbeat sync timer
    Modification to disable fw heartbeat sync timer by default for QAT Gen6
    HW. Added a module parameter to enable the sync timer feature for QAT
    Gen6 driver.
    
    Signed-off-by: George Abraham P <george.abraham.p@intel.com>

--- drivers/crypto/intel/qat/qat_6xxx/adf_6xxx_hw_data.c
+++ drivers/crypto/intel/qat/qat_6xxx/adf_6xxx_hw_data.c
@@ -81,6 +81,8 @@ static const unsigned long thrd_mask_dcpr[ADF_6XXX_MAX_ACCELENGINES] = {
 	0x2, 0x2, 0x2, 0x2, 0x2, 0x2, 0x2, 0x2, 0x00
 };
 
+static bool enable_hb_sync;
+
 static const char *const adf_6xxx_fw_objs[] = {
 	[ADF_FW_CY_OBJ] = ADF_6XXX_CY_OBJ,
 	[ADF_FW_DC_OBJ] = ADF_6XXX_DC_OBJ,
@@ -1008,8 +1010,10 @@ void adf_init_hw_data_6xxx(struct adf_hw_device_data *hw_data)
 	hw_data->bank_state_save = adf_bank_state_save;
 	hw_data->bank_state_restore = adf_bank_state_restore;
 	hw_data->num_hb_ctrs = ADF_NUM_HB_CNT_PER_AE;
-	hw_data->start_timer = adf_timer_start;
-	hw_data->stop_timer = adf_timer_stop;
+	if (enable_hb_sync) {
+		hw_data->start_timer = adf_timer_start;
+		hw_data->stop_timer = adf_timer_stop;
+	}
 	hw_data->init_device = adf_init_device;
 	hw_data->enable_pm = enable_pm;
 	hw_data->services_supported = services_supported;
@@ -1044,3 +1048,5 @@ void adf_clean_hw_data_6xxx(struct adf_hw_device_data *hw_data)
 
 module_param(enable_asym, bool, 0644);
 MODULE_PARM_DESC(enable_asym, "Enable asym support");
+module_param(enable_hb_sync, bool, 0644);
+MODULE_PARM_DESC(enable_hb_sync, "Enable FW heartbeat sync timer");
