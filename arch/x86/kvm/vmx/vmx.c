7edd8b819a3b8c8b058633d0f453f0681a59cf70
Tao Su <tao1.su@linux.intel.com>
2024-02-25 15:00:08 +0800

    EMBARGOED KVM: x86: Add new CPUID SGX256 definition
    The new SGX256 is enumerated via CPUID(EAX=0x12,ECX=0x0):EAX[12],
    indicating presence of EREPORT2, EGETKEY256.
    
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7988,6 +7988,7 @@ static __init void vmx_set_cpu_caps(void)
 		kvm_cpu_cap_clear(X86_FEATURE_SGX_LC);
 		kvm_cpu_cap_clear(X86_FEATURE_SGX1);
 		kvm_cpu_cap_clear(X86_FEATURE_SGX2);
+		kvm_cpu_cap_clear(X86_FEATURE_SGX256);
 		kvm_cpu_cap_clear(X86_FEATURE_SGX_EDECCSSA);
 	}
 
