4b6bb94a6641cea1b182f300a6b78ff099b8a3d3
Chang S. Bae <chang.seok.bae@intel.com>
2024-03-16 00:13:51 -0700

    x86/microcode/intel_staging: Support data transfer
    The staging architecture features a narrowed interface for data transfer.
    Instead of allocating MMIO space based on data chunk size, it utilizes
    two data registers: one for reading and one for writing, enforcing the
    serialization of read and write operations. Additionally, it defines a
    data format.
    
    To facilitate data transfer, implement helpers according to the specified
    format for writing or reading the staging data.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>

--- arch/x86/kernel/cpu/microcode/intel_staging.c
+++ arch/x86/kernel/cpu/microcode/intel_staging.c
@@ -8,6 +8,7 @@
 #define pr_fmt(fmt) "microcode: " fmt
 #include <linux/delay.h>
 #include <linux/io.h>
+#include <linux/pci_ids.h>
 
 #include "internal.h"
 
@@ -16,6 +17,8 @@
 
 #define MBOX_CONTROL_OFFSET	0x0
 #define MBOX_STATUS_OFFSET	0x4
+#define MBOX_WRDATA_OFFSET	0x8
+#define MBOX_RDDATA_OFFSET	0xc
 
 #define MASK_MBOX_CTRL_ABORT	BIT(0)
 #define MASK_MBOX_CTRL_GO	BIT(31)
@@ -24,10 +27,20 @@
 #define MASK_MBOX_STATUS_ERROR	BIT(2)
 #define MASK_MBOX_STATUS_READY	BIT(31)
 
+#define MASK_MBOX_RESP_SUCCESS	BIT(0)
+#define MASK_MBOX_RESP_PROGRESS	BIT(1)
+#define MASK_MBOX_RESP_ERROR	BIT(2)
+
+#define MBOX_CMD_LOAD		0x3
+#define MBOX_OBJ_STAGING	0xb
+#define MBOX_HDR		(PCI_VENDOR_ID_INTEL | (MBOX_OBJ_STAGING << 16))
+#define MBOX_HDR_SIZE		16
+#define MBOX_LOAD_LEN		PAGE_SIZE
 #define MBOX_MAX_LOAD(imgsz)	imgsz * 2
 #define MBOX_WAIT_TIMEOUT	10 * MSEC_PER_SEC
 
 #define STAGING_OFFSET_END	0xffffffff
+#define DWORD_SIZE(s)		s / sizeof(u32)
 
 /**
  * struct ucode_staging - Staging status to control the protocol
@@ -37,6 +50,7 @@
  * @img_offset	: the next offset in the payload image
  * @img_size	: the payload size
  * @load_len	: the size of the mailbox object being loaded
+ * @chunk_addr	: a pointer of the image being loaded
  */
 struct ucode_staging {
 	void __iomem	*base;
@@ -44,11 +58,41 @@ struct ucode_staging {
 	u32		img_offset;
 	u32		img_size;
 	u32		load_len;
+	u32		*chunk_addr;
 };
+
+static inline u32 read_dword(void __iomem *base)
+{
+	u32 dword = readl(base + MBOX_RDDATA_OFFSET);
+
+	/* Inform the read to the staging hardware */
+	writel(0, base + MBOX_RDDATA_OFFSET);
+	return dword;
+}
+
+static inline void write_dword(void __iomem *base, u32 dword)
+{
+	writel(dword, base + MBOX_WRDATA_OFFSET);
+}
 
 static void mbox_load_chunk(struct ucode_staging *stg)
 {
-	pr_debug_once("Need to implement Staging mailbox loading code.\n");
+	u32 *chunk = stg->chunk_addr;
+	u32 i, size;
+
+	stg->load_len = min(MBOX_LOAD_LEN, stg->img_size - stg->img_offset);
+	size = DWORD_SIZE(stg->load_len);
+
+	write_dword(stg->base, MBOX_HDR);
+	write_dword(stg->base, size + DWORD_SIZE(MBOX_HDR_SIZE));
+
+	write_dword(stg->base, MBOX_CMD_LOAD);
+	write_dword(stg->base, 0);
+
+	for (i = 0; i < size; i++)
+		write_dword(stg->base, chunk[i]);
+
+	writel(MASK_MBOX_CTRL_GO, stg->base + MBOX_CONTROL_OFFSET);
 }
 
 static enum ucode_state mbox_wait_xaction(struct ucode_staging *stg)
@@ -75,13 +119,23 @@ static enum ucode_state mbox_wait_xaction(struct ucode_staging *stg)
 
 static enum ucode_state mbox_read_resp(struct ucode_staging *stg)
 {
-	pr_debug_once("Need to implement Staging response handler.\n");
-	return UCODE_ERROR;
+	u32 flag;
+
+	WARN_ON_ONCE(read_dword(stg->base) != MBOX_HDR);
+	WARN_ON_ONCE(read_dword(stg->base) != DWORD_SIZE(MBOX_HDR_SIZE));
+
+	stg->img_offset = read_dword(stg->base);
+	stg->chunk_addr = stg->img_addr + stg->img_offset;
+
+	flag = read_dword(stg->base);
+	if (flag & MASK_MBOX_RESP_ERROR)
+		return UCODE_ERROR;
+	return UCODE_OK;
 }
 
 bool staging_work(u64 mmio_addr, void *payload, unsigned int size)
 {
-	struct ucode_staging stg = { .img_addr = payload, .img_size = size };
+	struct ucode_staging stg = { .img_addr = payload, .img_size = size, .chunk_addr = payload };
 	u32 mbox_load = 0, max_load = MBOX_MAX_LOAD(stg.img_size);
 	enum ucode_state state;
 
