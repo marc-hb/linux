867e99cf4d4e055c82d53919c519b29f55617024
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2024-06-03 16:02:52 +0800

    EDAC/igen6: Add Intel Arrow Lake-PS SoCs support
    Arrow Lake-PS SoCs share same IBECC registers with Meteor Lake-P
    SoCs. Add Arrow Lake-PS SoC compute die IDs for EDAC support.
    
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>

--- drivers/edac/igen6_edac.c
+++ drivers/edac/igen6_edac.c
@@ -268,6 +268,13 @@ static struct work_struct ecclog_work;
 #define DID_PTL_H_SKU2	0xb001
 #define DID_PTL_H_SKU3	0xb002
 
+/* Compute die IDs for Arrow Lake-PS with IBECC */
+#define DID_ARL_PS_SKU1	0x7d37
+#define DID_ARL_PS_SKU2	0x7d38
+#define DID_ARL_PS_SKU3	0x7d39
+#define DID_ARL_PS_SKU4	0x7d3a
+#define DID_ARL_PS_SKU5	0x7d3b
+
 static int get_mchbar(struct pci_dev *pdev, u64 *mchbar)
 {
 	union  {
@@ -610,6 +617,11 @@ static const struct pci_device_id igen6_pci_tbl[] = {
 	{ PCI_VDEVICE(INTEL, DID_ARL_UH_SKU1), (kernel_ulong_t)&mtl_p_cfg },
 	{ PCI_VDEVICE(INTEL, DID_ARL_UH_SKU2), (kernel_ulong_t)&mtl_p_cfg },
 	{ PCI_VDEVICE(INTEL, DID_ARL_UH_SKU3), (kernel_ulong_t)&mtl_p_cfg },
+	{ PCI_VDEVICE(INTEL, DID_ARL_PS_SKU1), (kernel_ulong_t)&mtl_p_cfg },
+	{ PCI_VDEVICE(INTEL, DID_ARL_PS_SKU2), (kernel_ulong_t)&mtl_p_cfg },
+	{ PCI_VDEVICE(INTEL, DID_ARL_PS_SKU3), (kernel_ulong_t)&mtl_p_cfg },
+	{ PCI_VDEVICE(INTEL, DID_ARL_PS_SKU4), (kernel_ulong_t)&mtl_p_cfg },
+	{ PCI_VDEVICE(INTEL, DID_ARL_PS_SKU5), (kernel_ulong_t)&mtl_p_cfg },
 	{ PCI_VDEVICE(INTEL, DID_PTL_H_SKU1), (kernel_ulong_t)&mtl_p_cfg },
 	{ PCI_VDEVICE(INTEL, DID_PTL_H_SKU2), (kernel_ulong_t)&mtl_p_cfg },
 	{ PCI_VDEVICE(INTEL, DID_PTL_H_SKU3), (kernel_ulong_t)&mtl_p_cfg },
