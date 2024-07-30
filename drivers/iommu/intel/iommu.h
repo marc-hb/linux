259dd33d268d3a506908a29b5981a2afa6ed81fd
Yang Zhong <yang.zhong@linux.intel.com>
2024-07-29 22:47:51 -0400

    iommu/vt-d: Only enable translated requests with PASID in SATS
    Refactor the context_set_pasid() function to extend the translated
    requests with PASID(bit6 EPTR) supported with SATS feature enabled
    in hardware and settings.
    
    Signed-off-by: Yang Zhong <yang.zhong@linux.intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>#cherry-pick only

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -61,7 +61,7 @@
 #define CONTEXT_TT_DEV_IOTLB	1
 #define CONTEXT_TT_PASS_THROUGH 2
 #define CONTEXT_PASIDE		BIT_ULL(3)
-
+#define CONTEXT_EPTR		BIT_ULL(6)
 /*
  * Intel IOMMU register specification per version 1.0 public spec.
  */
@@ -1025,12 +1025,10 @@ static inline void context_set_domain_id(struct context_entry *context,
 	context->hi |= (value & ((1 << 16) - 1)) << 8;
 }
 
-static inline void context_set_pasid(struct context_entry *context)
+static inline void context_set_pasid(struct context_entry *context,
+				     unsigned long value)
 {
-	context->lo |= CONTEXT_PASIDE;
-
-	/* EPTR (bit 6): Translated requests with PASID are allowed. */
-	context->lo |= BIT_ULL(6);
+	context->lo |= value;
 }
 
 static inline int context_domain_id(struct context_entry *c)
