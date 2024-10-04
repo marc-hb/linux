ce5fbdaf9a9c0dc826ee98c8adc69a43391e2f54
Jithu Joseph <jithu.joseph@intel.com>
2024-10-04 15:35:33 -0700

    x86/stop_machine: Add a new API stop_cluster_cpuslocked()
    stop_core_cpuslocked() is only good to handle SMT cases. In certain
    configurations where CPUs in a module need to be joining the rendevous, a
    different mask needs to be passed.
    
    Add a new API to call different cpumask's to call a handler.
    
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>

--- kernel/stop_machine.c
+++ kernel/stop_machine.c
@@ -652,6 +652,25 @@ int stop_core_cpuslocked(unsigned int cpu, cpu_stop_fn_t fn, void *data)
 EXPORT_SYMBOL_GPL(stop_core_cpuslocked);
 #endif
 
+int stop_cluster_cpuslocked(unsigned int cpu, cpu_stop_fn_t fn, void *data)
+{
+	const struct cpumask *cluster_mask = topology_cluster_cpumask(cpu);
+
+	struct multi_stop_data msdata = {
+		.fn = fn,
+		.data = data,
+		.num_threads = cpumask_weight(cluster_mask),
+		.active_cpus = cluster_mask,
+	};
+
+	lockdep_assert_cpus_held();
+
+	/* Set the initial state and stop all online cpus. */
+	set_state(&msdata, MULTI_STOP_PREPARE);
+	return stop_cpus(cluster_mask, multi_cpu_stop, &msdata);
+}
+EXPORT_SYMBOL_GPL(stop_cluster_cpuslocked);
+
 /**
  * stop_machine_from_inactive_cpu - stop_machine() from inactive CPU
  * @fn: the function to run
