2ead9e09da439c7ad178154c7e08e76389c77c41
Zide Chen <zide.chen@intel.com>
2024-12-17 10:23:47 -0800

    x86/fpu/xstate: Support independent supervisor feature for Intel PT
    When KVM enables Intel PT virtualization, it doesn't intercept most
    Intel PT state MSRs.  Since it allows Intel PT to operate on both host
    and guest, KVM needs to do guest/host context switch during VMX
    transitions.
    
    Added PT state to the independent supervisor features so that KVM can
    take advantage of xsaves/xrstors instructions to reduce overhead of
    save/restore Intel PT context.
    
    Signed-off-by: Zide Chen <zide.chen@intel.com>

--- arch/x86/include/asm/fpu/xstate.h
+++ arch/x86/include/asm/fpu/xstate.h
@@ -68,7 +68,7 @@
  * - Don't set the bit corresponding to the independent supervisor feature in
  *   IA32_XSS at run time, since it has been set at boot time.
  */
-#define XFEATURE_MASK_INDEPENDENT (XFEATURE_MASK_LBR)
+#define XFEATURE_MASK_INDEPENDENT (XFEATURE_MASK_LBR | XFEATURE_MASK_PT)
 
 /*
  * Unsupported supervisor features. When a supervisor feature in this mask is
