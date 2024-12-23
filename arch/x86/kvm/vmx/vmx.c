78c46c9878be7f353a974d2fa24255793eb37856
Zide Chen <zide.chen@intel.com>
2024-12-23 14:02:03 -0800

    KVM: VMX: Emulate IA32_RTIT_STATUS.PAUSED[8] bit
    ‘Paused’ bit is added to the IA32_RTIT_STATUS as part of PTTT.  Hardware
    sets this bit when a PTTT paused trigger action occurs and clears this
    bit when a PTTT resume trigger action occurs.
    
    Software that wants tracing to start only after reaching a trigger can
    manually set Paused=1 before setting the TraceEn bit.
    
    KVM should allow guest to program this bit.
    
    Signed-off-by: Zide Chen <zide.chen@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -160,10 +160,10 @@ module_param(enable_mediated_pmu, bool, 0444);
 
 #define RMODE_GUEST_OWNED_EFLAGS_BITS (~(X86_EFLAGS_IOPL | X86_EFLAGS_VM))
 
-#define MSR_IA32_RTIT_STATUS_MASK (~(RTIT_STATUS_FILTEREN | \
+#define RTIT_STATUS_VOLATILE_BITMASK (RTIT_STATUS_FILTEREN | \
 	RTIT_STATUS_CONTEXTEN | RTIT_STATUS_TRIGGEREN | \
 	RTIT_STATUS_ERROR | RTIT_STATUS_STOPPED | \
-	RTIT_STATUS_BYTECNT))
+	RTIT_STATUS_BYTECNT)
 
 /*
  * List of MSRs that can be directly passed to the guest.
@@ -2408,9 +2408,13 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		pt_update_intercept_for_msr(vcpu);
 		break;
 	case MSR_IA32_RTIT_STATUS:
+		u64 mask = RTIT_STATUS_VOLATILE_BITMASK;
+
 		if (!pt_can_write_msr(vmx))
 			return 1;
-		if (data & MSR_IA32_RTIT_STATUS_MASK)
+		if (vmx_guest_has_intel_pttt(vcpu))
+			mask |= RTIT_STATUS_PAUSED;
+		if (data & ~mask)
 			return 1;
 		vmx->pt_desc.guest.status = data;
 		break;
--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -698,6 +698,12 @@ static inline bool intel_pmu_lbr_is_enabled(struct kvm_vcpu *vcpu)
 	return !!vcpu_to_lbr_records(vcpu)->nr;
 }
 
+static inline bool vmx_guest_has_intel_pttt(struct kvm_vcpu *vcpu)
+{
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+	return intel_pt_validate_cap(vmx->pt_desc.caps, PT_CAP_trigger_tracing);
+}
+
 void intel_pmu_cross_mapped_check(struct kvm_pmu *pmu);
 int intel_pmu_create_guest_lbr_event(struct kvm_vcpu *vcpu);
 void vmx_passthrough_lbr_msrs(struct kvm_vcpu *vcpu);
