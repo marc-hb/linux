878791770e45da229717cb80743670ca24cbccd0
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2025-01-16 14:05:11 +0200

    i3c: mipi-i3c-hci-pci: Add support for Intel Meteor Lake-P I3C
    Add I3C controller PCI ID on Intel Meteor Lake-P.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>

--- drivers/i3c/master/mipi-i3c-hci/mipi-i3c-hci-pci.c
+++ drivers/i3c/master/mipi-i3c-hci/mipi-i3c-hci-pci.c
@@ -127,6 +127,8 @@ static const struct pci_device_id mipi_i3c_hci_pci_devices[] = {
 	/* Wildcat Lake-U */
 	{ PCI_VDEVICE(INTEL, 0x4d7c), (kernel_ulong_t)&intel_info},
 	{ PCI_VDEVICE(INTEL, 0x4d6f), (kernel_ulong_t)&intel_info},
+	/* Meteor Lake-P */
+	{ PCI_VDEVICE(INTEL, 0x7e7c), (kernel_ulong_t)&intel_info},
 	/* Panther Lake-H */
 	{ PCI_VDEVICE(INTEL, 0xe37c), (kernel_ulong_t)&intel_info},
 	{ PCI_VDEVICE(INTEL, 0xe36f), (kernel_ulong_t)&intel_info},
