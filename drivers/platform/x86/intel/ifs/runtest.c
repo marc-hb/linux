1bdc1a50e3e5c5d08c7aa3d0a4694babd5f48e68
Jithu Joseph <jithu.joseph@intel.com>
2025-02-12 16:24:10 -0800

    Revert "trace: platform/x86/intel/ifs: Add SBAF trace support"
    This reverts commit 61b74964536e86445d43acff5cff6ad907ba9321.
    
    Structural Based Functional Test at Field (SBAF) test was expected to be
    supported in Granite Rapids(GNR) cpus. However, due to recent roadmap
    changes, SBAF test support will not be included in GNR and is unlikely
    to be included in future cpus.
    
    Given this, remove the SBAF test from the In-Field Scan (IFS) driver.
    
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>

--- drivers/platform/x86/intel/ifs/runtest.c
+++ drivers/platform/x86/intel/ifs/runtest.c
@@ -528,7 +528,6 @@ static int dosbaf(void *data)
 	 */
 	wrmsrl(MSR_ACTIVATE_SBAF, run_params->activate->data);
 	rdmsrl(MSR_SBAF_STATUS, status.data);
-	trace_ifs_sbaf(ifsd->cur_batch, *run_params->activate, status);
 
 	/* Pass back the result of the test */
 	if (cpu == first)
