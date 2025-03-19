96b7df5da1438bda480f53bb1fbb49f8a6a594d8
Tao Su <tao1.su@linux.intel.com>
2025-03-19 11:17:07 +0800

    KVM: x86: Advertise AMX-* CPUID in leaf 0x1E to userspace
    Advertise new CPUID leaf 0x1E.1 and AMX-* CPUIDs.
    
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -1130,6 +1130,18 @@ void kvm_set_cpu_caps(void)
 		SCATTERED_F(SGX256),
 	);
 
+	kvm_cpu_cap_init(CPUID_1E_1_EAX,
+		F(AMX_INT8_MIRROR),
+		F(AMX_BF16_MIRROR),
+		F(AMX_COMPLEX_MIRROR),
+		F(AMX_FP16_MIRROR),
+		F(AMX_FP8),
+		F(AMX_TRANSPOSE),
+		F(AMX_TF32),
+		F(AMX_AVX512),
+		F(AMX_MOVRS),
+	);
+
 	kvm_cpu_cap_init(CPUID_24_0_EBX,
 		F(AVX10_128),
 		F(AVX10_256),
@@ -1708,6 +1720,20 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		if (!kvm_cpu_cap_has(X86_FEATURE_AMX_TILE)) {
 			entry->eax = entry->ebx = entry->ecx = entry->edx = 0;
 			break;
+		}
+
+		max_idx = entry->eax = min(entry->eax, 1u);
+
+		/* KVM only supports up to 0x1e.1, capped above via min(). */
+		if (max_idx >= 1) {
+			entry = do_host_cpuid(array, function, 1);
+			if (!entry)
+				goto out;
+
+			cpuid_entry_override(entry, CPUID_1E_1_EAX);
+			entry->ebx = 0;
+			entry->ecx = 0;
+			entry->edx = 0;
 		}
 		break;
 	/* Intel archPerfmon extended leaf */
