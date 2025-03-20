e47e9fcaaa74b8afadd7c45dbb290357fbaf7299
Mark Gross <mgross@linux.intel.com>
2025-03-20 10:57:54 -0700

    add build script used for creating initial BKC from a subset of intel-next pr's as documentation

---
+++ do-it-dmr.sh
@@ -0,0 +1,36 @@
+./merge.py -w \
+"rapl/idle,\
+SPI-NOR/PCI/Thunderbolt/USB4,\
+LPSS/SMBus,\
+cpuid_dmr_nis,\
+ifs_changes,\
+pcie6,\
+LASS,\
+edac,\
+edac_fixup,\
+fred_kvm,\
+fred_fixup,\
+mm_region,\
+KVM LASS,\
+ras_bff_dmr,\
+oobmsm,\
+avx_10,\
+sgx_sha384,\
+dmr_apx,\
+iommu_sats,\
+perf,\
+perf_fixup,\
+perf_tdx_kvm_support_fixup,\
+ucode_update,\
+kvm_cet,\
+nmi_source,\
+msr_instructions,\
+msr_instructions_fixup,\
+tdx_kvm_support,\
+tdx_kvm_support_fixup,\
+dsa_batchop_fix,\
+dsa3_dmr,\
+qat_support,\
+nmi_source_fixup"
+
+
