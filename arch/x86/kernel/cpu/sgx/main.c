225a60589104c3d486c05f63c9f79d4d220c89d6
Elena Reshetova <elena.reshetova@intel.com>
2025-05-22 12:21:38 +0300

    x86/sgx: Enable automatic SVN updates for SGX enclaves
    == Background ==
    
    ENCLS[EUPDATESVN] is a new SGX instruction [1] which allows enclave
    attestation to include information about updated microcode SVN without a
    reboot. Before an EUPDATESVN operation can be successful, all SGX memory
    (aka. EPC) must be marked as “unused” in the SGX hardware metadata
    (aka.EPCM). This requirement ensures that no compromised enclave can
    survive the EUPDATESVN procedure and provides an opportunity to generate
    new cryptographic assets.
    
    == Patch Contents ==
    
    Attempt to execute ENCLS[EUPDATESVN] every time the first file descriptor
    is obtained via sgx_(vepc_)open(). In the most common case the microcode
    SVN is already up-to-date, and the operation succeeds without updating SVN.
    If it fails with any other error code than SGX_INSUFFICIENT_ENTROPY, this
    is considered unexpected and the *open() returns an error. This should not
    happen in practice. On contrary, SGX_INSUFFICIENT_ENTROPY might happen due
    to a pressure on the system's DRNG (RDSEED) and therefore the *open() can
    be safely retried to allow normal enclave operation.
    
    [1] Runtime Microcode Updates with Intel Software Guard Extensions,
    https://cdrdv2.intel.com/v1/dl/getContent/648682
    
    Signed-off-by: Elena Reshetova <elena.reshetova@intel.com>

--- arch/x86/kernel/cpu/sgx/main.c
+++ arch/x86/kernel/cpu/sgx/main.c
@@ -940,6 +940,8 @@ EXPORT_SYMBOL_GPL(sgx_set_attribute);
 
 /* Counter to count the active SGX users */
 static atomic64_t sgx_usage_count;
+/* Mutex to ensure no concurrent EPC accesses during EUPDATESVN */
+static DEFINE_MUTEX(sgx_svn_lock);
 
 /**
  * sgx_updatesvn() - Attempt to call ENCLS[EUPDATESVN].
@@ -1009,8 +1011,37 @@ static int sgx_update_svn(void)
 
 int sgx_inc_usage_count(void)
 {
-	atomic64_inc(&sgx_usage_count);
-	return 0;
+	int ret;
+
+	/*
+	 * Increments from non-zero indicate potential other
+	 * active EPC users and EUPDATESVN is not attempted.
+	 */
+	if (atomic64_inc_not_zero(&sgx_usage_count))
+		return 0;
+
+	/*
+	 * Ensure no other concurrent threads can start
+	 * touching EPC while EUPDATESVN is running.
+	 */
+	guard(mutex)(&sgx_svn_lock);
+
+	if (atomic64_inc_not_zero(&sgx_usage_count))
+		return 0;
+
+	/*
+	 * Attempt to call EUPDATESVN since EPC must be
+	 * empty at this point.
+	 */
+	ret = sgx_update_svn();
+
+	/*
+	 * If EUPDATESVN failed, return failure to sgx_(vepc_)open and
+	 * do not increment the sgx_usage_count.
+	 */
+	if (!ret)
+		atomic64_inc(&sgx_usage_count);
+	return ret;
 }
 
 void sgx_dec_usage_count(void)
