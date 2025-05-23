03bb873663134b5f63020de29152109b5191f0e8
Zide Chen <zide.chen@intel.com>
2025-05-23 13:53:27 -0700

    Revert "KVM: x86/pmu: Check PMU cpuid configuration from user space"
    This patch was rejected by upstream and the KVM maintainer wants to
    vet vCPUID only for the sake of KVM safety but not the general sanity
    checks.
    
    Signed-off-by: Zide Chen <zide.chen@intel.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -199,16 +199,7 @@ static int kvm_check_cpuid(struct kvm_vcpu *vcpu)
 		union cpuid10_eax eax;
 		union cpuid10_edx edx;
 
-		eax.full = best->eax;
 		edx.full = best->edx;
-		if (enable_mediated_pmu &&
-		    eax.split.version_id > kvm_pmu_cap.version)
-			return -EINVAL;
-		if (eax.split.version_id > 0 && !vcpu_pmu_can_enable(vcpu))
-			return -EINVAL;
-		if (eax.split.version_id > 1 && eax.split.version_id < 5 &&
-		    best->ecx != 0)
-			return -EINVAL;
 		if (eax.split.version_id >= 5) {
 			int mask_all = (1 << edx.split.num_counters_fixed) - 1;
 
--- arch/x86/kvm/pmu.c
+++ arch/x86/kvm/pmu.c
@@ -816,11 +816,6 @@ static void kvm_pmu_reset(struct kvm_vcpu *vcpu)
 	kvm_pmu_call(reset)(vcpu);
 }
 
-inline bool vcpu_pmu_can_enable(struct kvm_vcpu *vcpu)
-{
-	return vcpu->kvm->arch.enable_pmu && lapic_in_kernel(vcpu);
-}
-
 /*
  * Refresh the PMU configuration for the vCPU, e.g. if userspace changes CPUID
  * and/or PERF_CAPABILITIES.
@@ -853,7 +848,8 @@ void kvm_pmu_refresh(struct kvm_vcpu *vcpu)
 	pmu->pebs_data_cfg_rsvd = ~0ull;
 	bitmap_zero(pmu->all_valid_pmc_idx, X86_PMC_IDX_MAX);
 
-	if (!vcpu_pmu_can_enable(vcpu))
+	if (!vcpu->kvm->arch.enable_pmu ||
+	    (!lapic_in_kernel(vcpu) && enable_mediated_pmu))
 		return;
 
 	kvm_pmu_call(refresh)(vcpu);
