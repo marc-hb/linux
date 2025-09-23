marc-hb/linux

commit f79b1c573cb4dc551919f81ed5797419f6fc1f3a

x86/i8237: Register device based on FADT legacy boot flag
From Skylake onwards, the platform controller hub (Sunrisepoint PCH) does
not support legacy DMA operations to IO ports 81h-83h, 87h, 89h-8Bh, 8Fh.
Currently this driver registers as syscore ops and its resume function is
called on every resume from S3. On Skylake and Kabylake, this causes a
resume delay of around 100ms due to port IO operations, which is a problem.

This change allows to load the driver only when the platform bios
explicitly supports such devices or has a cut-off date earlier than 2017
due to the following reasons:

   - The platforms released before year 2017 have support for the 8237.
     (except Sunrisepoint PCH e.g. Skylake)

   - Some of the BIOS that were released for platforms (Skylake, Kabylake)
     during 2016-17 are buggy. These BIOS do not set/unset the
     ACPI_FADT_LEGACY_DEVICES field in FADT table properly based on the
     presence or absence of the DMA device.

Very recently, open source system firmware like coreboot started unsetting
ACPI_FADT_LEGACY_DEVICES field in FADT table if the 8237 DMA device is not
present on the PCH.

Please refer to chapter 21 of 6th Generation Intel® Core™ Processor
Platform Controller Hub Family: BIOS Specification.

Signed-off-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@intel.com>
Signed-off-by: Anshuman Gupta <anshuman.gupta@intel.com>
Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
Cc: rjw@rjwysocki.net
Cc: hpa@zytor.com
Cc: Alan Cox <alan@linux.intel.com>
Link: https://lkml.kernel.org/r/1522336015-22994-1-git-send-email-anshuman.gupta@intel.com

arch/x86/kernel/i8237.c
@@ -9,10 +9,12 @@
  * your option) any later version.
  */
 
+#include <linux/dmi.h>
 #include <linux/init.h>
 #include <linux/syscore_ops.h>
 
 #include <asm/dma.h>
+#include <asm/x86_init.h>
 
 /*
  * This module just handles suspend/resume issues with the
@@ -49,6 +51,29 @@ static struct syscore_ops i8237_syscore_ops = {
 
 static int __init i8237A_init_ops(void)
 {
+	/*
+	 * From SKL PCH onwards, the legacy DMA device is removed in which the
+	 * I/O ports (81h-83h, 87h, 89h-8Bh, 8Fh) related to it are removed
+	 * as well. All removed ports must return 0xff for a inb() request.
+	 *
+	 * Note: DMA_PAGE_2 (port 0x81) should not be checked for detecting
+	 * the presence of DMA device since it may be used by BIOS to decode
+	 * LPC traffic for POST codes. Original LPC only decodes one byte of
+	 * port 0x80 but some BIOS may choose to enhance PCH LPC port 0x8x
+	 * decoding.
+	 */
+	if (dma_inb(DMA_PAGE_0) == 0xFF)
+		return -ENODEV;
+
+	/*
+	 * It is not required to load this driver as newer SoC may not
+	 * support 8237 DMA or bus mastering from LPC. Platform firmware
+	 * must announce the support for such legacy devices via
+	 * ACPI_FADT_LEGACY_DEVICES field in FADT table.
+	 */
+	if (x86_pnpbios_disabled() && dmi_get_bios_year() >= 2017)
+		return -ENODEV;
+
 	register_syscore_ops(&i8237_syscore_ops);
 	return 0;
 }

arch/x86/include/asm/x86_init.h
@@ -301,5 +301,6 @@ extern struct x86_apic_ops x86_apic_ops;
 extern void x86_early_init_platform_quirks(void);
 extern void x86_init_noop(void);
 extern void x86_init_uint_noop(unsigned int unused);
+extern bool x86_pnpbios_disabled(void);
 
 #endif

arch/x86/kernel/platform-quirks.c
@@ -33,9 +33,14 @@ void __init x86_early_init_platform_quirks(void)
 		x86_platform.set_legacy_features();
 }
 
+bool __init x86_pnpbios_disabled(void)
+{
+	return x86_platform.legacy.devices.pnpbios == 0;
+}
+
 #if defined(CONFIG_PNPBIOS)
 bool __init arch_pnpbios_disabled(void)
 {
-	return x86_platform.legacy.devices.pnpbios == 0;
+	return x86_pnpbios_disabled();
 }
 #endif

commit 5ed518118b8d89603f413c738421343857ff8a9e

x86/i8237: Move FADT legacy device check before port I/O access
Commit f79b1c573c added support to allow enumeration of 8237 DMA device
only if BIOS exposes support via ACPI_FADT_LEGACY_DEVICES field in FADT
table. It also added an additional check using dma_inb(DMA_PAGE_0)
returning 0xFF as a means to detect unsupported devices (mainly for
broken BIOS versions).

However, the port I/O access on unsupported IO address can trigger
Completer Abort (CA) errors on some platforms, which may cause Machine
Check Exceptions (MCE) when CA error reporting is enabled. This is
problematic for platforms like Diamond Rapids where MCE is enabled
for CA errors.

Move the BIOS FADT flag check to the beginning of the initialization
function to avoid unnecessary port access on platforms where BIOS
properly configures ACPI_FADT_LEGACY_DEVICES support. This reduces the
likelihood of triggering CA errors while maintaining the fallback
detection mechanism for platforms with broken BIOS implementations.

Fixes: f79b1c573cb4 ("x86/i8237: Register device based on FADT legacy boot flag")
Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

arch/x86/kernel/i8237.c
@@ -48,6 +48,15 @@ static struct syscore_ops i8237_syscore_ops = {
 static int __init i8237A_init_ops(void)
 {
 	/*
+	 * It is not required to load this driver as newer SoC may not
+	 * support 8237 DMA or bus mastering from LPC. Platform firmware
+	 * must announce the support for such legacy devices via
+	 * ACPI_FADT_LEGACY_DEVICES field in FADT table.
+	 */
+	if (x86_pnpbios_disabled() && dmi_get_bios_year() >= 2017)
+		return -ENODEV;
+
+	/*
 	 * From SKL PCH onwards, the legacy DMA device is removed in which the
 	 * I/O ports (81h-83h, 87h, 89h-8Bh, 8Fh) related to it are removed
 	 * as well. All removed ports must return 0xff for a inb() request.
@@ -59,15 +68,6 @@ static int __init i8237A_init_ops(void)
 	 * decoding.
 	 */
 	if (dma_inb(DMA_PAGE_0) == 0xFF)
-		return -ENODEV;
-
-	/*
-	 * It is not required to load this driver as newer SoC may not
-	 * support 8237 DMA or bus mastering from LPC. Platform firmware
-	 * must announce the support for such legacy devices via
-	 * ACPI_FADT_LEGACY_DEVICES field in FADT table.
-	 */
-	if (x86_pnpbios_disabled() && dmi_get_bios_year() >= 2017)
 		return -ENODEV;
 
 	register_syscore_ops(&i8237_syscore_ops);